7a1f49e65e9bf16fda47b1101173a545be5c45ff
Eli Spicer <joshua.spicer.2020@student.lhschools.org>
2020-03-13 12:09:32 -0400

    UserDataCaptured

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,50 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+   modulusClock();
+
+
     }
+    public static void modulusClock(){
+        Scanner modulusClock;
+        modulusClock = new Scanner(System.in);
+
+    System.out.println("Give me a number of hours");
+    int hour = modulusClock.nextInt();
+
+    System.out.println("Enter a value for minutes");
+    int minutes = modulusClock.nextInt();
+
+    System.out.println(hour + " : " + minutes);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
